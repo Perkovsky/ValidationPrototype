f36d454e7e1b9d7840d16f01c7def21b05e2676d
Igor Perkovsky <igor.perkovsky@mail.ru>
2021-05-25 17:20:50 +0300

    PermissionAccessValidationMiddleware has been modified

--- ValidationPrototype/Middlewares/PermissionAccessValidationMiddleware.cs
+++ ValidationPrototype/Middlewares/PermissionAccessValidationMiddleware.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ValidationPrototype.Services;
 
 namespace ValidationPrototype.Middlewares
 {
 	public class PermissionAccessValidationMiddleware
 	{
 		private readonly RequestDelegate _next;
+		private readonly ILogger<PermissionAccessValidationMiddleware> _logger;
 
-		public PermissionAccessValidationMiddleware(RequestDelegate next) => _next = next;
+		public PermissionAccessValidationMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+		{
+			_next = next;
+			_logger = loggerFactory?.CreateLogger<PermissionAccessValidationMiddleware>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+		}
 
-		public async Task InvokeAsync(HttpContext context, IEntityValidationService identityService)
+		public async Task InvokeAsync(HttpContext context/*, IPermissionAccessValidationService ValidationService*/)
 		{
 			await _next.Invoke(context);
 		}
