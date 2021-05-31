463a9040734112399aac59db87e806fb589e6717
Igor Perkovsky <igor.perkovsky@mail.ru>
2021-05-31 23:00:11 +0300

    isPrimitiveLogicValidationFaulted has been added

--- ValidationPrototype/Validators/BaseValidator.cs
+++ ValidationPrototype/Validators/BaseValidator.cs
@@ -1,4 +1,6 @@
 ﻿using FluentValidation;
+using System;
+using System.Linq.Expressions;
 using ValidationPrototype.Services;
 
 namespace ValidationPrototype.Validators
@@ -9,14 +11,21 @@ namespace ValidationPrototype.Validators
 	{
 		private readonly IValidationService<T> _validationService;
 
+		protected bool isPrimitiveLogicValidationFaulted;
+
 		public BaseValidator(IValidationService<T> validationService)
 		{
 			_validationService = validationService;
 
-			CascadeMode = CascadeMode.Stop;
-
 			PrimitiveLogicValidation();
 			BusinessLogicValidation();
+		}
+
+		public new IRuleBuilderOptions<T, TProperty> RuleFor<TProperty>(Expression<Func<T, TProperty>> expression)
+		{
+			return base.RuleFor(expression)
+				.Must(x => true)
+				.OnAnyFailure(x => isPrimitiveLogicValidationFaulted = true);
 		}
 
 		protected virtual void PrimitiveLogicValidation()
@@ -25,8 +34,7 @@ namespace ValidationPrototype.Validators
 
 		protected virtual void BusinessLogicValidation()
 		{
-			RuleFor(x => x)
-				.Must(x => _validationService.Validate(x));
+			When(x => !isPrimitiveLogicValidationFaulted, () => base.RuleFor(x => x).Must(x => _validationService.Validate(x)));
 		}
 	}
 }
